4b7237508d197bb4fdbebc8219bad9bfe23327aa
Warwick Molloy <wmolloy@gmail.com>
2018-09-08 21:59:36 +1000

    Build environment now uses least dev dependencies; it prefixes the final files with a version string and puts them in the package directory - use of /dist is now dropped.

--- post-dist.js
+++ post-dist.js
@@ -18,4 +18,53 @@ function copyBrowserFileToLiveDocDir() {
   fs.copyFileSync(fileToCopy, destination)
 }
 
+function prefixVersion(content) {
+  let version = process.env.npm_package_version
+  return `//  Fluid-DOM v ${version}
+${content}
+`
+}
+
+function asyncRead(fname) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.readFile(fname, (err, data) => {
+        if (err) { reject(err) } else { resolve(data.toString()) }
+      })
+    } catch(e) {
+      resolve(e)
+    }
+  })
+}
+
+function asyncWrite(fname, data) {
+  return new Promise( (resolve, reject) => {
+    fs.writeFile(fname, data, (err) => reject(err))
+    resolve()
+  })
+}
+
+function addVersionComment(fname) {
+  asyncRead(fname)
+  .then( x=> prefixVersion(x) )
+  .then( last => 
+    asyncWrite(fname, last)
+  )
+  .catch( error => console.error( error ))
+}
+
+function findFluidAndAddVersionComment() {
+  fs.readdir('.', (err, files)=> {
+    if (err) {
+      console.warn(`Could not access file listing: ${err}`)
+    } else {
+      for(var a_file of files.filter(x => x.indexOf('fluid') != -1)) {
+        addVersionComment(a_file)
+        console.log(`--> ${a_file}`)
+      }
+    }
+  })
+}
+
+findFluidAndAddVersionComment()
 copyBrowserFileToLiveDocDir()
