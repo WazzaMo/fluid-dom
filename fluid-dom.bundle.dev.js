6b12e75aa5f8736c8ff9095add966bc1b7380294
Warwick Molloy <wmolloy@gmail.com>
2018-09-08 22:54:56 +1000

    updated file list

--- fluid-dom.bundle.dev.js
+++ fluid-dom.bundle.dev.js
@@ -1,4 +1,4 @@
-//  Fluid-DOM v 1.1.2
+//  Fluid-DOM v 1.1.3
 var fluid = (function (exports) {
     'use strict';
 
--- fluid-dom.commonjs.dev.js
+++ fluid-dom.commonjs.dev.js
@@ -1,4 +1,4 @@
-//  Fluid-DOM v 1.1.2
+//  Fluid-DOM v 1.1.3
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
